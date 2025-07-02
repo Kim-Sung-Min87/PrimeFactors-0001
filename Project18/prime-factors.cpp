9a5622b89f58df873711fcfbe66d733ff0451feb
sungmini.kim <clever1501@naver.com>
2025-07-02 10:01:25 +0900

    [feature] page 1

--- Project18/prime-factors.cpp
+++ Project18/prime-factors.cpp
@@ -1,6 +1,13 @@
+#include <vector>
 
+using std::vector;
 
 class PrimeFactors {
+private:
 
-
+public:
+	vector<int> of(int n) {
+		
+		return {};
+	}
 };
--- Project18/prime-factors_test.cpp
+++ Project18/prime-factors_test.cpp
@@ -1,8 +1,13 @@
 #include "gmock/gmock.h"
 #include "prime-factors.cpp"
+#include <vector>
 
 using namespace testing;
 
-TEST(PrimeFactors, PrimeTest) {
-	EXPECT_EQ(1, 1);
+TEST(PrimeFactors, Of1) {
+
+	PrimeFactors prime_factors;
+	vector<int> expected = {};
+
+	EXPECT_EQ(expected, prime_factors.of(1));
 }
